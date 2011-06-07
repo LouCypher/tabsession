6346c4a429771fd177fef109a9ee5a7066068a55
zoolcar9 <zoolcar9@f8bc7839-dd48-0410-b6ac-112692871d22>
2011-06-07 10:34:19 +0000

    * Fixed: Tab Options conflicted with Tab Mix Plus extension * This is the last version to support Firefox 3.* - No more Flock and Songbird supports

--- chrome/content/tabSession.js
+++ chrome/content/tabSession.js
@@ -11,7 +11,7 @@
   for the specific language governing rights and limitations under the
   License.
 
-  The Original Code is Tab Seesion History extension.
+  The Original Code is Tab Session History extension.
 
   The Initial Developer of the Original Code is LouCypher.
   Portions created by the Initial Developer are Copyright (C) 2007
