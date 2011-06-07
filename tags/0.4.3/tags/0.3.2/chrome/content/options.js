e845c085f5dedd8ba43c9e952cc3fba8646a7c8f
zoolcar9 <zoolcar9@f8bc7839-dd48-0410-b6ac-112692871d22>
2011-06-07 10:44:12 +0000

    The last version to support Firefox 3.* and older

---
+++ tags/0.4.3/tags/0.3.2/chrome/content/options.js
@@ -0,0 +1,51 @@
+var TabSession_Config = {
+  get prefService() {
+    return Components.classes["@mozilla.org/preferences-service;1"]
+                     .getService(Components.interfaces.nsIPrefService);
+  },
+
+  get prefs() {
+    return this.prefService.getBranch("extensions.Tab_Session_History.");
+  },
+
+  get tabbox() {
+    return document.getElementsByTagName("tabbox")[0];
+  },
+
+  disableBaFo: function(aBoolean) {
+    var id = ["extensions.Tab_Session_History.showMenu.contentBack-check",
+              "extensions.Tab_Session_History.showMenu.contentForward-check"];
+    var checkbox;
+    for (var i in id) {
+      checkbox = document.getElementById(id[i]);
+      checkbox.disabled = aBoolean;
+    }
+  },
+
+  setLastTab: function() {
+    this.prefs.setIntPref("options.lastTab", this.tabbox._tabs.selectedIndex);
+  },
+
+  getLastTab: function() {
+    try {
+      return this.prefs.getIntPref("options.lastTab");
+    } catch(ex) {
+      return 0;
+    }
+  },
+
+  init: function() {
+    var allowHidingContent = this.prefs.getBoolPref("allowHidingContentBackForward");
+    this.disableBaFo(!allowHidingContent);
+    this.tabbox._tabs.selectedIndex = this.getLastTab();
+  }
+}
+
+window.addEventListener("load", function(e) {
+  TabSession_Config.init();
+}, false);
+
+window.addEventListener("unload", function(e) {
+  TabSession_Config.setLastTab();
+}, false);
+
